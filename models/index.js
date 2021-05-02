5d69800ef67996a5c505bc43f27944d2eaaaad73
markeezy <markkhoo95@gmail.com>
2021-05-02 12:47:32 -0700

    add models

---
+++ models/index.js
@@ -0,0 +1,38 @@
+//import models
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+//
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//export models
+module.exports = { 
+    Comment, 
+    Post,
+    User,
+};
