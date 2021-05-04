8f9888350cff5c80ac7e960b8ae22de998cb1a09
markeezy <markkhoo95@gmail.com>
2021-05-03 18:50:05 -0700

    attempt login auth check

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        //
+        // Get all Posts data
         const postData = await Post.findAll({
             include: [
                 {
@@ -13,14 +14,26 @@ router.get('/', async (req, res) => {
             ],
         });
 
-        //
+        // Serialize data so the template can read it
         const posts = postData.map((arg) => arg.get({ plain: true }));
 
-        //
+        // Pass serialized data for handlebar
         res.render('homepage', {
             posts
         });
 
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+router.get('/login', async (req, res) => {
+    try {
+        // Catches incase user is logged in already
+
+
+        // Render login else
+        res.render('login');
     } catch (err) {
         res.status(500).json(err);
     };
