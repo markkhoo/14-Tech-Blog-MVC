18455204a08e5a51c8e83b04e2ed7f47bef7891c
markeezy <markkhoo95@gmail.com>
2021-05-03 18:18:04 -0700

    add first fetch and helpers for handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,29 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
 
+router.get('/', async (req, res) => {
+    try {
+        //
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        //
+        const posts = postData.map((arg) => arg.get({ plain: true }));
+
+        //
+        res.render('homepage', {
+            posts
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const exphbs = require('express-handlebars');
 // 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 // 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3002;
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 // 
 const sess = {
